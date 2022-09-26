700b0a946a1377a2ac99c8f345728758ed2095f9
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-09-26 14:31:50 +0300

    M.A. created validations

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   # GET /birds
   def index
     birds = Bird.all
@@ -9,7 +10,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
 
@@ -47,4 +48,7 @@ class BirdsController < ApplicationController
     render json: { error: "Bird not found" }, status: :not_found
   end
 
+  def render_unprocessable_entity_response(invalid)
+    render json:{error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
 end
